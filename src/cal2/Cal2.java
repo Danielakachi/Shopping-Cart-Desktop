d69015f7fb0eea1332bc0780c60a9a207313f9c1
Daniel AKachi <akachidanil@gmail.com>
2017-11-06 04:42:03 +0100

    first commit

---
+++ src/cal2/Cal2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cal2;
+
+/**
+ *
+ * @author Dell E6540
+ */
+public class Cal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        hop1 h=new hop1();
+        h.setVisible(true);
+        h.setSize(900, 900);
+        
+    }
+    
+}
---
+++ src/cal2/comma.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cal2;
+
+/**
+ *
+ * @author Dell E6540
+ */
+public class comma {
+    String value;
+    
+    public String commaAdd(String x)
+    {
+        int value=x.length();
+        return x;
+    }
+    
+    
+}
